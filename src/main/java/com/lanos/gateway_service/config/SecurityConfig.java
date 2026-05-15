74ad0bcc64be5b25486d35c235726cf9a32afb6e
riya95-techdev <nriya0804@gmail.com>
2026-05-15 16:37:58 +0530

    first commit

---
+++ src/main/java/com/lanos/gateway_service/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.lanos.gateway_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+	@Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeExchange(exchange -> exchange
+
+                        .pathMatchers("/public/**").permitAll()
+
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt());
+//                .httpBasic(basic -> basic.disable())
+//                .formLogin(form -> form.disable());
+
+        return http.build();
+    }
+}
